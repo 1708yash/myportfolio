39410446820592a52772777b15a17ce15fd0ed0d
Yash <140030060+teamsProjectsem5@users.noreply.github.com>
2023-10-26 09:57:39 +0530

    header complete

--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -5,8 +5,9 @@ import React from "react";
 import Button from "./Button";
 import {HiMenu} from 'react-icons/hi'
 import {IoCloseSharp} from 'react-icons/io5'
+import { SocialIcon } from "react-social-icons";
 function Header() {
-  const Links = [
+  let Links = [
     { name: "Home", link: "/" },
     { name: "Projects", link: "/" },
     { name: "About", link: "/about" },
@@ -15,16 +16,41 @@ function Header() {
   const [open, setOpen] = React.useState(false)
   return (
     <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-5">
         <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray800">
-          <span className="text-3xl text-indigo-600 mr-1 pt-2">
-            <Image src="/assets/ylogo.png" width={50} height={50} alt="logo" />
-          </span>
-          Designer
+          <ul className="flex items-center justify-center mx-2 left-0 gap-3">
+            <li className="">
+              <SocialIcon
+                url="https://github.com/1708yash"
+                style={{ width: "32px", height: "32px" }}
+              />
+            </li>
+            <li>
+              {" "}
+              <SocialIcon
+                url="https://www.linkedin.com/in/yash-tomar1708/"
+                style={{ width: "32px", height: "32px" }}
+              />
+            </li>
+            <li>
+              {" "}
+              <SocialIcon
+                url="https://www.instagram.com/tomar___yash/"
+                style={{ width: "32px", height: "32px" }}
+              />
+            </li>
+            <li>
+              {" "}
+              <SocialIcon
+                url="https://www.youtube.com/channel/UCzjSbcwh-tmDIRuyynn8Lvw"
+                style={{ width: "32px", height: "32px" }}
+              />
+            </li>
+          </ul>
         </div>
         <div
           onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+          className="text-3xl absolute right-8 top-5 cursor-pointer md:hidden"
         >
           <HiMenu className={`${open ? "hidden" : "visible"}`} />
           <IoCloseSharp className={`${open ? "visible" : "hidden"}`} />
@@ -32,7 +58,7 @@ function Header() {
         <ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
             open ? "top-20 opacity-100" : "top-[-490px]"
-          } opacity-0 md:opacity-0100`}
+          } opacity-0 md:opacity-100`}
         >
           {Links.map((link) => (
             <li className="md:ml-8 text-xl md:my-0 my-7" key={link.name}>
